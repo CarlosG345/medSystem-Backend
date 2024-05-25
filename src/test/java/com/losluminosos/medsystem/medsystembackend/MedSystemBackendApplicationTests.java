006f5eba4fe92e43aa149d93913fba95fc5aea5f
CarlosG345 <u202210364@upc.edu.pe>
2024-05-25 18:34:00 -0500

    chore: initial commit

---
+++ src/test/java/com/losluminosos/medsystem/medsystembackend/MedSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.losluminosos.medsystem.medsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
